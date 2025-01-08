1ecb27c2db1b348c8af33347014dde4dec529b3a
jeremie <nerom95@gmail.com>
2025-01-08 18:28:49 +0100

    nav mobile visibilite au scroll et ajout composent bouton

--- src/app/components/header/navMobile.tsx
+++ src/app/components/header/navMobile.tsx
@@ -1,16 +1,18 @@
 "use client";
 import { AnimatePresence, motion } from "motion/react";
-import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { useEffect, useRef, useState } from "react";
-import { CgMenuRight } from "react-icons/cg";
+import { CgMenu } from "react-icons/cg";
 import { FiX } from "react-icons/fi";
+import { Button } from "../buttons/buttons";
 import ToggleTheme from "../buttons/toggleTheme";
 import { nav } from "./navigation";
 
 export default function NavMobile() {
   const pathname = usePathname();
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isVisible, setIsVisible] = useState(true);
+  const [lastScrollY, setLastScrollY] = useState(0);
   const toggleMenu = () => setIsMenuOpen(!isMenuOpen);
 
   const menuRef = useRef<HTMLDivElement>(null);
@@ -28,16 +30,41 @@ export default function NavMobile() {
     };
   }, []);
 
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollY = window.scrollY;
+      if (currentScrollY > lastScrollY && currentScrollY > 100) {
+        setIsVisible(false);
+      } else {
+        setIsVisible(true);
+      }
+      setLastScrollY(currentScrollY);
+    };
+
+    if (isMenuOpen && !isVisible) {
+      setIsMenuOpen(false);
+    }
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [lastScrollY, isMenuOpen, isVisible]);
+
   return (
-    <nav>
-      <button onClick={toggleMenu} className="text-2xl place-self-end">
-        {!isMenuOpen && <CgMenuRight className="size-[1.8rem]" />}
-      </button>
-      <AnimatePresence initial={false}>
+    <>
+      <div>
+        {!isMenuOpen && (
+          <button
+            onClick={toggleMenu}
+            className="text-2xl place-self-end border-2 border-border rounded-md rounded-tr-2xl p-2"
+          >
+            <CgMenu className="size-[1.8rem]" />
+          </button>
+        )}
+      </div>
+      <AnimatePresence>
         {isMenuOpen && (
           <motion.div
             ref={menuRef}
-            className="fixed top-0 right-0 h-full w-[280px] bg-background shadow shadow-primary border-l-2 border-primary/40 p-4 flex flex-col items-start justify-start gap-4 rounded-l-lg z-50"
+            className="absolute top-0 z-10 right-0 w-[280px]"
             initial={{ opacity: 0, x: "100%" }}
             animate={{
               opacity: 1,
@@ -52,32 +79,36 @@ export default function NavMobile() {
               duration: 2,
             }}
           >
-            <button onClick={toggleMenu} className="text-2xl self-end">
-              <FiX />
-            </button>
-            <ul className="flex flex-col items-start gap-6">
-              {nav.map((item) => {
-                const isActive = pathname === item.href;
-                return (
-                  <li
-                    key={item.title}
-                    className={`w-fit text-foreground  py-1 px-2 rounded-md ${
-                      isActive ? "bg-primary/20" : "hover:bg-primary/20"
-                    }`}
-                  >
-                    <Link onClick={toggleMenu} href={item.href}>
-                      {item.title}
-                    </Link>
-                  </li>
-                );
-              })}
-            </ul>
-            <div className="flex items-center gap-4">
-              <ToggleTheme />
+            <div className="bg-background/90 border-2 border-border/20 border-r-0 rounded-l-lg p-4 flex flex-col items-start justify-start gap-16 h-screen backdrop-blur-sm">
+              <div className="w-full flex items-center justify-between">
+                <ToggleTheme />
+                <Button onClick={toggleMenu} theme="icon">
+                  <FiX className="size-[1.5rem]" />
+                </Button>
+              </div>
+              <nav>
+                <ul className="flex flex-col items-start gap-4">
+                  {nav.map((item) => {
+                    const isActive = pathname === item.href;
+                    return (
+                      <li key={item.title}>
+                        <Button
+                          onClick={toggleMenu}
+                          href={item.href}
+                          isActive={isActive}
+                          theme="highlight"
+                        >
+                          {item.title}
+                        </Button>
+                      </li>
+                    );
+                  })}
+                </ul>
+              </nav>
             </div>
           </motion.div>
         )}
       </AnimatePresence>
-    </nav>
+    </>
   );
 }
