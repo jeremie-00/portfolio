606b3d85e6d2b0c323cc01cad113f75f9a5b2cc6
jeremie <nerom95@gmail.com>
2025-01-08 18:31:35 +0100

    page home: ajout des composents

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,15 @@
-import Image from "next/image";
+"use client";
+import { Hero } from "./components/pages/home/hero";
+import Skills from "./components/pages/home/skills";
+import { ParticlesBackground } from "./components/particles";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center py-2">
-      <h1 className="text-6xl font-bold">
-        Welcome to <span className="text-primary-500">Next.js</span>
-      </h1>
-
-      <Image
-        className="mx-auto mt-8"
-        src="/vercel.svg"
-        alt="Vercel Logo"
-        width={200}
-        height={100}
-      />
-
-      <p className="mt-6 text-center text-gray-500">
-        Get started by editing
-        <code className="px-2 py-1 rounded bg-gray-100 text-gray-800">
-          src/app/page.tsx
-        </code>
-      </p>
-    </main>
+    <>
+      <ParticlesBackground />
+      <Hero />
+      <Skills />
+      <section className="relative mt-20"></section>
+    </>
   );
 }
