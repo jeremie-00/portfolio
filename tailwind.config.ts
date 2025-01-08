7327e3199bff89fdb6fde2cf4fc9238e5128e4e9
jeremie <nerom95@gmail.com>
2025-01-08 18:36:12 +0100

    Tailwind conf : ajout de linear gradian, section bg, border, secondary card et plugin button

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+import plugin from "tailwindcss/plugin";
 
 export default {
   darkMode: ["class"],
@@ -9,20 +10,41 @@ export default {
   ],
   theme: {
     extend: {
+      rotate: { "negative-3": "-3deg" },
       backgroundImage: {
         "linear-custom":
-          "linear-gradient(to bottom, var(--tw-gradient-from), var(--tw-gradient-to))",
+          "linear-gradient(to top, var(--tw-gradient-from), var(--tw-gradient-to))",
       },
       colors: {
         background: "hsl(var(--background))",
         foreground: "hsl(var(--foreground))",
         transition: "hsl(var(--transition-bg))",
+        section: "hsl(var(--section-bg))",
+        border: "hsl(var(--border))",
         primary: {
           DEFAULT: "hsl(var(--primary))",
           foreground: "hsl(var(--primary-foreground))",
         },
+        secondary: {
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
+        },
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
+        },
       },
     },
   },
-  plugins: [],
+  plugins: [
+    plugin(function ({ addBase, addComponents }) {
+      addBase({});
+      addComponents({
+        ".button": {
+          "@apply flex gap-2 rounded-lg shadow-md transition-all duration-300 ease-in-out":
+            {},
+        },
+      });
+    }),
+  ],
 } satisfies Config;
