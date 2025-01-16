9ef7eff21c3e61c0aae9d88eca0e14d7fa9ba40b
jeremie <nerom95@gmail.com>
2025-01-16 06:52:32 +0100

    Logo : remplacement image

--- src/app/components/header/logo.tsx
+++ src/app/components/header/logo.tsx
@@ -7,8 +7,13 @@ export default function Logo() {
       href="/"
       className="flex items-center hover:scale-105 transition-scale duration-300"
     >
+      {/* <div className="flex items-end gap-2 text-2xl font-bold">
+        Jerem
+        <span className="text-primary w-3 h-3 rounded-full border-2 border-primary bg-primary"></span>
+      </div> */}
+
       <Image
-        src={"/favicon.ico"}
+        src={"/favicon-192x192.png"}
         alt={"Logo H"}
         width={150}
         height={150}
