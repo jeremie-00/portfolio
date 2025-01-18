9ea12f74fe74011796776fbf09779d70b3f57554
jeremie <nerom95@gmail.com>
2025-01-18 09:21:37 +0100

    Staire : modification de l'animation transform translateY a la place de position top

--- src/app/components/transitionPages/stairs.tsx
+++ src/app/components/transitionPages/stairs.tsx
@@ -2,13 +2,13 @@ import { motion } from "motion/react";
 
 const stairAnimation = {
   initial: {
-    top: "0%",
+    transform: "translateY(0%)",
   },
   animate: {
-    top: "100%",
+    transform: "translateY(100%)",
   },
   exit: {
-    top: ["100%", "0%"],
+    transform: "translateY(0%)",
   },
 };
 
