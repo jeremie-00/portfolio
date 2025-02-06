6a71183a7d999fe2aa6660ad73abc9816ca2d0a8
jeremie <nerom95@gmail.com>
2025-02-06 09:43:27 +0100

    Seed section

--- prisma/seed-section.ts
+++ prisma/seed-section.ts
@@ -4,12 +4,51 @@ const prisma = new PrismaClient();
 
 export const sections = [
   {
+    page: 0,
+    order: 1,
+    href: "Accueil",
+    title: "title Accueil",
+    subtitle: "subtitle des accueil",
+    description: "description des accueil",
+    image: {
+      url: "https://tx41fcbjdi0olrou.public.blob.vercel-storage.com/skills/1731739323739-5473-LDrbdAXPmmntzt5cPCnABZ1BS7aQLf.svg",
+      alt: "Image",
+      position: "left",
+    },
+  },
+  {
+    page: 0,
+    order: 2,
+    href: "A propos de moi",
+    title: "title propos",
+    subtitle: "subtitle des propos",
+    description: "description des propos",
+    image: {
+      url: "https://tx41fcbjdi0olrou.public.blob.vercel-storage.com/skills/1731739323739-5473-LDrbdAXPmmntzt5cPCnABZ1BS7aQLf.svg",
+      alt: "Image",
+      position: "left",
+    },
+  },
+  {
+    page: 0,
+    order: 3,
+    href: "Mon parcours",
+    title: "title Accueparcoursil",
+    subtitle: "subtitle des parcours",
+    description: "description des parcours",
+    image: {
+      url: "https://tx41fcbjdi0olrou.public.blob.vercel-storage.com/skills/1731739323739-5473-LDrbdAXPmmntzt5cPCnABZ1BS7aQLf.svg",
+      alt: "Image",
+      position: "left",
+    },
+  },
+  {
     page: 1,
     order: 1,
-    name: "Technologies",
-    title: "title Technologies",
-    subtitle: "subtitle des technologies",
-    description: "description des technologies",
+    href: "A propos de moi",
+    title: "title a propos",
+    subtitle: "subtitle des a propos",
+    description: "description des a propos",
     image: {
       url: "https://tx41fcbjdi0olrou.public.blob.vercel-storage.com/skills/1731739323739-5473-LDrbdAXPmmntzt5cPCnABZ1BS7aQLf.svg",
       alt: "Image",
@@ -19,10 +58,10 @@ export const sections = [
   {
     page: 1,
     order: 2,
-    name: "Design",
-    title: "title Design",
-    subtitle: "subtitle des Design",
-    description: "description des Design",
+    href: "Mes technologies favorites",
+    title: "title technologies favorites",
+    subtitle: "subtitle des technologies favorites",
+    description: "description des technologies favorites",
     image: {
       url: "https://tx41fcbjdi0olrou.public.blob.vercel-storage.com/skills/1731739323739-5473-LDrbdAXPmmntzt5cPCnABZ1BS7aQLf.svg",
       alt: "Image",
@@ -32,10 +71,10 @@ export const sections = [
   {
     page: 1,
     order: 3,
-    name: "Finance",
-    title: "title Finance",
-    subtitle: "subtitle des Finance",
-    description: "description des Finance",
+    href: "Mon profil",
+    title: "title profil",
+    subtitle: "subtitle des profil",
+    description: "description des profil",
     image: {
       url: "https://tx41fcbjdi0olrou.public.blob.vercel-storage.com/skills/1731739323739-5473-LDrbdAXPmmntzt5cPCnABZ1BS7aQLf.svg",
       alt: "Image",
@@ -43,9 +82,14 @@ export const sections = [
     },
   },
   {
+    page: 1,
+    order: 4,
+    href: "Mes hobbies",
+  },
+  {
     page: 2,
     order: 1,
-    name: "Design",
+    href: "Design",
     title: "title Design",
     subtitle: "subtitle des Design",
     description: "description des Design",
@@ -57,7 +101,7 @@ export const sections = [
   {
     page: 3,
     order: 1,
-    name: "Marketing",
+    href: "Marketing",
     title: "title Marketing",
     subtitle: "subtitle des Marketing",
     description: "description des Marketing",
@@ -69,7 +113,7 @@ export const sections = [
   {
     page: 4,
     order: 1,
-    name: "Finance",
+    href: "Finance",
     title: "title Finance",
     subtitle: "subtitle des Finance",
     description: "description des Finance",
@@ -78,23 +122,31 @@ export const sections = [
       alt: "Image",
     },
   },
+  {
+    page: 5,
+    order: 1,
+    href: "Dashboard",
+    title: "title Dashboard",
+    subtitle: "subtitle des Dashboard",
+    description: "description des Dashboard",
+  },
 ];
 
 async function main() {
   console.log("Seeding database...");
 
   for (const section of sections) {
-    const { name, description, image, page, order, title, subtitle } = section;
+    const { href, description, image, page, order, title, subtitle } = section;
 
     await prisma.section.create({
       data: {
-        name,
+        href,
         title,
         subtitle,
         description,
         page,
         order,
-        image: {
+        image: image && {
           create: {
             url: image.url,
             alt: image.alt,
