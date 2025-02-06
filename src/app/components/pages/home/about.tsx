cc315a10c6db37f5cbd9524c5aebc7974da419d4
jeremie <nerom95@gmail.com>
2025-02-06 09:59:33 +0100

    Refactor about component

--- src/app/components/pages/home/about.tsx
+++ src/app/components/pages/home/about.tsx
@@ -1,36 +1,34 @@
+"use client";
 import { Container, Content } from "../../containers";
 
 import { useIsMobile } from "@/app/hooks/useMobile";
-import { getAbout } from "@/app/services/about.actions";
-import { useEffect, useState } from "react";
+import { AboutProps } from "@/app/services/about.actions";
+import { SectionType } from "@/app/types/prismaType";
 import { ChatBull, ChatBullProps } from "../../ChatBull";
 
-export default function About() {
-  const [texts, setTexts] = useState<string[]>([]);
-  const [image, setImage] = useState<string>();
+export default function About({
+  section,
+  about,
+}: {
+  section: SectionType;
+  about: AboutProps;
+}) {
   const isMobile = useIsMobile();
   const positionArrow: ChatBullProps["arrowPosition"][] = isMobile
     ? ["middleBottomLeft", "middleBottomRight"]
     : ["bottomLeft", "bottomRight"];
-  useEffect(() => {
-    const fetchTexts = async () => {
-      const about = await getAbout();
-      setTexts(about.texts);
-      setImage(about.image);
-    };
-    fetchTexts();
-  }, []);
+
   return (
     <section className="section flex-col justify-center items-center">
       <Container>
-        <h2 className="h2">A propos de moi</h2>
+        <h2 className="h2">{section.title}</h2>
         <div className="bg-image-svg rounded-xl">
           <Content className="md:p-16 p-4 rounded-xl border-primary/50 border bg-background/20">
-            {texts.map((text, index) => (
+            {about.texts.map((text, index) => (
               <ChatBull
                 key={index}
                 text={text}
-                image={image}
+                image={about.image}
                 arrowPosition={
                   index % 2 === 0 ? positionArrow[0] : positionArrow[1]
                 }
