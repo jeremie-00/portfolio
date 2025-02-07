b4362ac7d6878a773fc0fd7086e98133c8882a67
jeremie <nerom95@gmail.com>
2025-02-07 18:40:24 +0100

    Navigation action

--- src/app/services/navigation.actions.ts
+++ src/app/services/navigation.actions.ts
@@ -1,22 +1,189 @@
-export interface NavLinksProps {
-  page: string;
-  href: string;
-  title: string;
-}
+"use server";
+import { z } from "zod";
+import { zfd } from "zod-form-data";
+import prisma from "../lib/prisma";
+import { authActionClient } from "../lib/safe-action";
+import { Result } from "../types/globalType";
+import { FullLink } from "../types/prismaType";
+import { deleteSchema } from "../types/zodType";
 
-const nav: NavLinksProps[] = [
-  { href: "/", title: "Accueil", page: "accueil" },
-  { href: "/pages/projet", title: "Mes Projets", page: "projet" },
-  { href: "/pages/contact", title: "Me contacter", page: "contact" },
-  { href: "/login", title: "Connexion", page: "login" },
-  { href: "/pages/legal", title: "Mention légale", page: "legal" },
-];
-
-// Fonction pour récupérer tous les projets
-export const getNavLinks = async (): Promise<NavLinksProps[]> => {
-  // Simulez un appel à une base de données
-  return new Promise((resolve) => {
-    const filteredNav = nav.filter((link) => link.page !== "legal");
-    resolve(filteredNav);
+export const getNavLinks = async (): Promise<FullLink[]> => {
+  return await prisma.link.findMany({
+    orderBy: {
+      order: "asc",
+    },
   });
 };
+
+export const getNavLinksExceptLegal = async (): Promise<FullLink[]> => {
+  return await prisma.link.findMany({
+    where: {
+      page: {
+        not: "legal",
+      },
+    },
+    orderBy: {
+      order: "asc",
+    },
+  });
+};
+
+export const getNavLinksById = async (id: string) => {
+  return await prisma.link.findUnique({
+    where: {
+      id: id,
+    },
+  });
+};
+export const getNavLinksByPage = async (page: string) => {
+  return await prisma.link.findUnique({
+    where: {
+      page: page,
+    },
+  });
+};
+
+const navigationSchemaCreate = zfd.formData({
+  page: z.string().nonempty({ message: "Vous devez fournir une page" }),
+
+  href: z
+    .string()
+    .nonempty({ message: "Vous devez fournir un href" })
+    .refine((url) => /^\/.*$/.test(url) || /^https?:\/\//.test(url), {
+      message: "L'URL doit être une URL valide ou un chemin relatif",
+    }),
+  title: z.string().nonempty({ message: "Vous devez fournir un titre" }),
+  order: z.string().nonempty({ message: "Vous devez fournir un ordre" }),
+});
+
+export const createNavigation = authActionClient
+  .schema(navigationSchemaCreate)
+  .action(async ({ parsedInput: { ...formData } }) => {
+    try {
+      const { page, href, title, order } = formData;
+
+      if (!page || !href || !title) {
+        return {
+          success: false,
+          status: "warn",
+          message: `Vous devez fournir un page, un href et un titre.`,
+        };
+      }
+
+      await prisma.link.create({
+        data: {
+          page: page,
+          href: href,
+          title: title,
+          order: Number(order),
+        },
+      });
+
+      return {
+        success: true,
+        status: "success",
+        message: `La navigation a été créée avec succès ! 🚀`,
+      };
+    } catch {
+      return {
+        success: false,
+        status: "error",
+        message: "Erreur lors de la création de la navigation.",
+      };
+    }
+  }) as (formData: FormData) => Promise<Result>;
+
+const navigationSchemaUpdate = zfd.formData({
+  id: z.string().nonempty({ message: "Vous devez fournir un identifiant" }),
+  page: z.string().nonempty({ message: "Vous devez fournir une page" }),
+  href: z
+    .string()
+    .nonempty({ message: "Vous devez fournir un href" })
+    .refine((url) => /^\/.*$/.test(url) || /^https?:\/\//.test(url), {
+      message: "L'URL doit être une URL valide ou un chemin relatif",
+    }),
+  title: z.string().nonempty({ message: "Vous devez fournir un titre" }),
+  order: z.string().nonempty({ message: "Vous devez fournir un ordre" }),
+});
+
+export const updateNavigation = authActionClient
+  .schema(navigationSchemaUpdate)
+  .action(async ({ parsedInput: { ...formData } }) => {
+    try {
+      const { id, page, href, title, order } = formData;
+
+      const existingNavigation = await getNavLinksById(id);
+
+      if (!existingNavigation) {
+        return {
+          success: false,
+          status: "warn",
+          message: "Navigation introuvable.",
+        };
+      }
+
+      if (!page || !href || !title) {
+        return {
+          success: false,
+          status: "warn",
+          message: `Vous devez fournir un page, un href et un titre.`,
+        };
+      }
+
+      await prisma.link.update({
+        where: {
+          id: id,
+        },
+        data: {
+          page: page,
+          href: href,
+          title: title,
+          order: Number(order),
+        },
+      });
+
+      return {
+        success: true,
+        status: "success",
+        message: `La navigation a été mise à jour avec succès ! 🚀`,
+      };
+    } catch {
+      return {
+        success: false,
+        status: "error",
+        message: "Erreur lors de la mise à jour de la navigation.",
+      };
+    }
+  }) as (formData: FormData) => Promise<Result>;
+
+export const deleteNavigation = authActionClient
+  .schema(deleteSchema)
+  .action(async ({ parsedInput: { id } }) => {
+    try {
+      const deleteNavigation = await getNavLinksById(id);
+      if (!deleteNavigation) {
+        return {
+          success: false,
+          status: "warn",
+          message: "Navigation introuvable.",
+        };
+      }
+      await prisma.link.delete({
+        where: {
+          id: id,
+        },
+      });
+
+      return {
+        success: true,
+        status: "success",
+        message: `La navigation a été supprimée avec succès ! 🚀`,
+      };
+    } catch {
+      return {
+        success: false,
+        status: "error",
+        message: "Erreur lors de la suppression de la navigation.",
+      };
+    }
+  }) as ({ id }: { id: string }) => Promise<Result>;
