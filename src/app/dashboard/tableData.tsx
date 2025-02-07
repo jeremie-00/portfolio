e0d5b42a400599c8c713e0730885f4371b069ced
jeremie <nerom95@gmail.com>
2025-02-07 11:13:04 +0100

    Table data border et bg

--- src/app/dashboard/tableData.tsx
+++ src/app/dashboard/tableData.tsx
@@ -147,150 +147,145 @@ export function TableData<TData extends { id: string }>({
 
   return (
     <div className="w-full">
-      <div className="w-full">
-        <div className="flex items-center py-4">
-          <input
-            placeholder={`Filter ${filter}...`}
-            value={(table.getColumn(filter)?.getFilterValue() as string) ?? ""}
-            onChange={(event) =>
-              table.getColumn(filter)?.setFilterValue(event.target.value)
-            }
-            className="max-w-sm"
+      <div className="flex items-center py-4">
+        <input
+          placeholder={`Filter ${filter}...`}
+          value={(table.getColumn(filter)?.getFilterValue() as string) ?? ""}
+          onChange={(event) =>
+            table.getColumn(filter)?.setFilterValue(event.target.value)
+          }
+          className="max-w-sm"
+        />
+
+        <div className="w-full flex items-center justify-center gap-4">
+          <Button
+            theme="primary"
+            size="sm"
+            onClick={handleShowForm}
+            disabled={loading}
+            ariaLabel="Ajouter"
+          >
+            <IoAdd size={24} />
+          </Button>
+
+          <DeleteButton
+            loading={loadingDelete}
+            selectedIds={selectedIds}
+            confirmDelete={confirmDelete}
           />
 
-          <div className="w-full flex items-center justify-center gap-4">
-            <Button
-              theme="primary"
-              size="sm"
-              onClick={handleShowForm}
-              disabled={loading}
-              ariaLabel="Ajouter"
-            >
-              <IoAdd size={24} />
-            </Button>
-
-            <DeleteButton
+          {showDialog && (
+            <DialogDelete
+              handleDelete={handleDelete}
+              cancelDelete={cancelDelete}
               loading={loadingDelete}
-              selectedIds={selectedIds}
-              confirmDelete={confirmDelete}
             />
+          )}
+        </div>
 
-            {showDialog && (
-              <DialogDelete
-                handleDelete={handleDelete}
-                cancelDelete={cancelDelete}
-                loading={loadingDelete}
-              />
-            )}
-          </div>
-
-          <DropdownMenu>
-            <DropdownMenuTrigger asChild>
-              <div className="w-full flex flex-1 items-center justify-end">
-                <Button theme="outline" size="sm" className="ml-auto">
-                  Columns <ChevronDown />
-                </Button>
-              </div>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent align="end">
-              {table
-                .getAllColumns()
-                .filter((column) => column.getCanHide())
-                .map((column) => {
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <div className="w-full flex flex-1 items-center justify-end">
+              <Button theme="outline" size="sm" className="ml-auto">
+                Columns <ChevronDown />
+              </Button>
+            </div>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            {table
+              .getAllColumns()
+              .filter((column) => column.getCanHide())
+              .map((column) => {
+                return (
+                  <DropdownMenuCheckboxItem
+                    key={column.id}
+                    className="capitalize"
+                    checked={column.getIsVisible()}
+                    onCheckedChange={(value) =>
+                      column.toggleVisibility(!!value)
+                    }
+                  >
+                    {column.id}
+                  </DropdownMenuCheckboxItem>
+                );
+              })}
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
+      <div className="rounded-md border border-foreground bg-section">
+        <Table>
+          <TableHeader>
+            {table.getHeaderGroups().map((headerGroup) => (
+              <TableRow key={headerGroup.id}>
+                {headerGroup.headers.map((header) => {
                   return (
-                    <DropdownMenuCheckboxItem
-                      key={column.id}
-                      className="capitalize"
-                      checked={column.getIsVisible()}
-                      onCheckedChange={(value) =>
-                        column.toggleVisibility(!!value)
-                      }
-                    >
-                      {column.id}
-                    </DropdownMenuCheckboxItem>
+                    <TableHead key={header.id}>
+                      {header.isPlaceholder
+                        ? null
+                        : flexRender(
+                            header.column.columnDef.header,
+                            header.getContext()
+                          )}
+                    </TableHead>
                   );
                 })}
-            </DropdownMenuContent>
-          </DropdownMenu>
-        </div>
-        <div className="rounded-md border">
-          <Table>
-            <TableHeader>
-              {table.getHeaderGroups().map((headerGroup) => (
-                <TableRow key={headerGroup.id}>
-                  {headerGroup.headers.map((header) => {
-                    return (
-                      <TableHead key={header.id}>
-                        {header.isPlaceholder
-                          ? null
-                          : flexRender(
-                              header.column.columnDef.header,
-                              header.getContext()
-                            )}
-                      </TableHead>
-                    );
-                  })}
+              </TableRow>
+            ))}
+          </TableHeader>
+          <TableBody>
+            {table.getRowModel().rows?.length ? (
+              table.getRowModel().rows.map((row) => (
+                <TableRow
+                  key={row.id}
+                  data-state={row.getIsSelected() && "selected"}
+                >
+                  {row.getVisibleCells().map((cell) => (
+                    <TableCell key={cell.id}>
+                      {flexRender(
+                        cell.column.columnDef.cell,
+                        cell.getContext()
+                      )}
+                    </TableCell>
+                  ))}
                 </TableRow>
-              ))}
-            </TableHeader>
-            <TableBody>
-              {table.getRowModel().rows?.length ? (
-                table.getRowModel().rows.map((row) => (
-                  <TableRow
-                    key={row.id}
-                    data-state={row.getIsSelected() && "selected"}
-                  >
-                    {row.getVisibleCells().map((cell) => (
-                      <TableCell key={cell.id}>
-                        {flexRender(
-                          cell.column.columnDef.cell,
-                          cell.getContext()
-                        )}
-                      </TableCell>
-                    ))}
-                  </TableRow>
-                ))
-              ) : (
-                <TableRow>
-                  <TableCell
-                    colSpan={columns.length}
-                    className="h-24 text-center"
-                  >
-                    {loading ? "Chargement..." : "No results."}
-                  </TableCell>
-                </TableRow>
-              )}
-            </TableBody>
-          </Table>
+              ))
+            ) : (
+              <TableRow>
+                <TableCell
+                  colSpan={columns.length}
+                  className="h-24 text-center"
+                >
+                  {loading ? "Chargement..." : "No results."}
+                </TableCell>
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
+      </div>
+      <div className="flex items-center justify-end space-x-2 py-4">
+        <div className="flex-1 text-sm text-muted-foreground">
+          {table.getFilteredSelectedRowModel().rows.length} of{" "}
+          {table.getFilteredRowModel().rows.length} row(s) selected.
         </div>
-        <div className="flex items-center justify-end space-x-2 py-4">
-          <div className="flex-1 text-sm text-muted-foreground">
-            {table.getFilteredSelectedRowModel().rows.length} of{" "}
-            {table.getFilteredRowModel().rows.length} row(s) selected.
-          </div>
-          <div className="flex space-x-2">
-            <Button
-              theme="outline"
-              size="sm"
-              onClick={() => table.previousPage()}
-              disabled={!table.getCanPreviousPage()}
-            >
-              Previous
-            </Button>
-            <Button
-              theme="outline"
-              size="sm"
-              onClick={() => table.nextPage()}
-              disabled={!table.getCanNextPage()}
-            >
-              Next
-            </Button>
-          </div>
+        <div className="flex space-x-2">
+          <Button
+            theme="outline"
+            size="sm"
+            onClick={() => table.previousPage()}
+            disabled={!table.getCanPreviousPage()}
+          >
+            Previous
+          </Button>
+          <Button
+            theme="outline"
+            size="sm"
+            onClick={() => table.nextPage()}
+            disabled={!table.getCanNextPage()}
+          >
+            Next
+          </Button>
         </div>
       </div>
-      {/*       <Button theme="outline" size="sm" onClick={onClick}>
-        Formulaire
-      </Button> */}
     </div>
   );
 }
